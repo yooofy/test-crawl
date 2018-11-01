d0103e48c915f1a672622781d80f5ac58c72a7d3
yoofy <yoofy.liu@sunvalley.com.cn>
2018-11-01 14:33:14 +0800

    增加日志

--- src/main/java/com/yoofy/crawl/CrawlApplication.java
+++ src/main/java/com/yoofy/crawl/CrawlApplication.java
@@ -1,6 +1,8 @@
 package com.yoofy.crawl;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,8 +13,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @MapperScan({"com.yoofy.crawl.dao", "com.yoofy.crawl.daoEX"})
 public class CrawlApplication {
+    private static Logger logger = LoggerFactory.getLogger(CrawlApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(CrawlApplication.class, args);
+        logger.info("service start");
     }
 }
